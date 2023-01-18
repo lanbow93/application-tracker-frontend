22c8b94723452f74590a48251696f17e76d19a03
Sabrina Chen <sabrinaxchen2016@gmail.com>
2023-01-17 19:51:50 -0700

    added h1, jobs applied on Index page

--- src/pages/Index.js
+++ src/pages/Index.js
@@ -1,6 +1,22 @@
+import {Form, Link, userLoaderData} from 'react-router-dom'
+
 function Index(prop){
+    const jobs=userLoaderData()
     return(
-        <h1>Index</h1>
+        <div>
+            <h1>My Job Tracker</h1>
+            <div className="index">
+            {jobs.map((job)=>{      
+                <div>          
+                    <h2>{`${job.jobTitle}`}</h2>
+                    <h3>{`${job.companyName}`}</h3>
+                    <h3>{`${job.salary}`}</h3>
+                    <Link to={`/${job._id}`} className='job'>Click for details</Link>
+                </div>   
+            })}
+        </div>
+       </div>
+        
     )
 }
 
