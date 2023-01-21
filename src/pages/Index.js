445cf6f9397635561fe67c1a8a298157a22a1441
Sabrina Chen <sabrinaxchen2016@gmail.com>
2023-01-21 12:47:27 -0700

    added NewPost component, jobs in Index page, and job details in Show page

--- src/pages/Index.js
+++ src/pages/Index.js
@@ -6,12 +6,18 @@ function Index(prop){
         <div>
             <h1>My Job Tracker</h1>
             <div className="index">
+                <div className='newPost'>
+                    <NewPost />
+                </div>
             {jobs.map((job)=>{      
                 <div>          
                     <h2>{`${job.jobTitle}`}</h2>
                     <h3>{`${job.companyName}`}</h3>
                     <h3>{`${job.salary}`}</h3>
                     <Link to={`/${job._id}`} className='job'>Click for details</Link>
+                    <Form action={`/delete/${job._id}`} method='post'>
+                        <input type='submit' value={'X'}/>
+                    </Form>
                 </div>   
             })}
         </div>
--- src/pages/Show.js
+++ src/pages/Show.js
@@ -1,6 +1,31 @@
+import { useLoaderData } from "react-router-dom"
+import {Link} from 'react-router-dom'
+import {Form} from 'react-router-dom'
+
 function Show(prop){
+    const job=useLoaderData()
+    console.log(job)
     return(
-        <h1>Show</h1>
+        <div className='job'>
+            <h1>{job.companyName}</h1>
+            <h2>{job.jobTitle}</h2>
+            <h3>{job.comments}</h3>
+            <Link to='/'>
+                <div>Home</div>
+            </Link>
+            <div>
+                <input type='button' value="Add to Favorites" />
+            </div>
+            <div>
+                <Link to={`/update/${job._id}`}>
+                    <div>Update</div>
+                </Link>
+            </div>
+            <Link to={`${job.link}`}>
+                <div>Apply</div>
+            </Link>
+            
+        </div>
     )
 }
 
