547d69f840afd2ef1445479288d93f930d0f3362
Christian Mpabuka <christianmpabuka@gmail.com>
2023-01-21 13:55:46 -0600

    router.js

---
+++ src/router.js
@@ -0,0 +1,22 @@
+import { createBrowserRouter, createRoutesFromElements, Route,} from "react-router-dom"
+import App from "./App"
+import { jobTrackerLoader } from "./loaders"
+import Index from "./pages/Index"
+import UpdateForm from "./components/UpdateForm"
+import { createAction, deleteAction, updateAction } from "./action"
+
+
+const router = createBrowserRouter (
+
+    createRoutesFromElements (
+    <Route path="" element={<App/>}>
+        <Route path="" element={<Index/>} loader={jobTrackerLoader}/>
+        <Route path="/create" action={createAction}/>
+        <Route path="/:id/delete" action={deleteAction}/>
+        <Route path="/:id/edit" element={<UpdateForm/>}/>
+        <Route path="/id:/update" action={updateAction}/>
+    </Route>
+    )
+)
+
+export default router
